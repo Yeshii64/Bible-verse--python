4508a192d077fa6dbe2867b24b440b221930bfb0
Yeshii64 <yeshuaafflick5@gmail.com>
2025-04-22 15:35:24 -0400

    going to take a nap - tested the notification module.. it worked! now i am creating the function to get the random Bible verse!

--- BibleVerse.py
+++ BibleVerse.py
@@ -1,1 +1,21 @@
 #Bible verse reminder!
+import requests
+import time
+from notifypy import Notify as np
+
+#testing out the notification
+def notication():
+    npq = np()
+    npq.title = "testing 123"
+    npq.message = "git rocks!"
+    npq.send()
+    time.sleep(8)
+    return
+
+#function to get a random Bible verse
+def getBibleVerse():
+    url = "https://bible-api.com/"
+    response = requests.get(url)
+    #checking if the status code is good
+    if response.status_code == 200:
+        data = response.json
